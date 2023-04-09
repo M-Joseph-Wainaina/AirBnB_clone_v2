849a69a1e109b10eaf3aea0da6faf6944e218dcb
Your Name <you@example.com>
2023-04-09 21:00:12 +0000

    changed shebang

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 Write a Fabric script that generates a .tgz archive
         from the contents of the web_static folder
@@ -7,6 +7,7 @@ Write a Fabric script that generates a .tgz archive
 """
 from datetime import datetime
 from fabric.api import local
+
 
 def do_pack():
     """ function that converts the content of webstatic into a .tgz
